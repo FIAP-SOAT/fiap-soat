b7afd6b887666eb6b79c2e58d82d5c9a8c5db080
Yago Inacio <yagofaran@gmail.com>
2023-06-27 17:26:38 -0300

    list status http adapter

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@shared/adapter/database/database.module';
 import { CreateOrderController } from './adapter/http/controllers/createOrder.controller';
 import { CreateOrderUseCase } from './application/services/createOrderUseCase';
+import { ListStatusController } from './adapter/http/controllers/listStatus.controller';
+import { ListStatusUseCase } from './application/services/listStatusUseCase';
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [CreateOrderController],
-  providers: [CreateOrderUseCase],
+  controllers: [CreateOrderController, ListStatusController],
+  providers: [CreateOrderUseCase, ListStatusUseCase],
 })
 export class OrdersModule {}
